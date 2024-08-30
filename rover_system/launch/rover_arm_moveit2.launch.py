f08ffa4cd6abd3bc118de6b86e871e7861946f7e
mn297 <peterwayne3293@gmail.com>
2024-08-29 22:18:32 -0400

    first code

---
+++ rover_system/launch/rover_arm_moveit2.launch.py
@@ -0,0 +1,202 @@
+# TODO FIXME implement the launch file for the arm_moveit2.launch.py
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    ExecuteProcess,
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from ament_index_python.packages import get_package_share_directory
+import os
+import tempfile
+import xacro
+
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.conditions import IfCondition, UnlessCondition
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+from moveit_configs_utils import MoveItConfigsBuilder
+
+
+def generate_launch_description():
+    moveit_config = (
+        MoveItConfigsBuilder("your_custom_arm_package")
+        .robot_description(
+            file_path="config/your_custom_arm.urdf.xacro"
+        )  # Update this line
+        .trajectory_execution(file_path="config/gripper_moveit_controllers.yaml")
+        .planning_scene_monitor(
+            publish_robot_description=True, publish_robot_description_semantic=True
+        )
+        .planning_pipelines(
+            pipelines=["ompl", "chomp", "pilz_industrial_motion_planner"]
+        )
+        .to_moveit_configs()
+    )
+
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
+
+    world_file_path = os.path.join(
+        get_package_share_directory("rover_system"),
+        "model",
+        "worlds",
+        "world.sdf",
+    )
+
+    # Process xacro or urdf for robot_description
+    xacro_file_path = os.path.join(
+        get_package_share_directory("rover_system"),
+        "model",
+        # "two_wheel_robot.xacro",
+        "rover.urdf.xacro",
+    )
+
+    urdf_file_path = os.path.join(
+        get_package_share_directory("rover_system"),
+        "model",
+        # "two_wheel_robot.urdf",
+        "rover.urdf",
+    )
+
+    # Method 1
+    # robot_description_config = xacro.process_file(xacro_file_path)
+    # robot_desc = robot_description_config.toxml()
+    # Method 2
+    robot_desc = ParameterValue(Command(["xacro ", xacro_file_path]), value_type=str)
+    # robot_desc = ParameterValue(Command(["xacro ", urdf_file_path]), value_type=str)
+
+    # Xacro command to generate URDF
+    xacro_command = [
+        "ros2",
+        "run",
+        "xacro",
+        "xacro",
+        xacro_file_path,
+        "-o",
+        urdf_file_path,
+    ]
+
+    temp_urdf_file = tempfile.NamedTemporaryFile(delete=False, mode="w", suffix=".urdf")
+    if os.path.exists(temp_urdf_file.name):
+        os.remove(temp_urdf_file.name)
+    urdf_file_path = temp_urdf_file.name
+
+    # Convert XACRO to URDF and write to the temporary file
+    command = f"xacro {xacro_file_path} > {urdf_file_path}"
+    convert_xacro_to_urdf = ExecuteProcess(
+        cmd=[command],
+        shell=True,
+        output="screen",
+    )
+
+    # Start Ignition Gazebo with an empty world
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="true",
+        description="Use simulation (Gazebo) clock if true",
+    )
+
+    # Gazebo simulation
+    world = LaunchConfiguration("world")
+    declare_world_cmd = DeclareLaunchArgument(
+        "world", default_value="maze.sdf", description="World file to use in Gazebo"
+    )
+    gz_world_arg = PathJoinSubstitution(
+        [get_package_share_directory("rover_system"), "model", "worlds", world]
+    )
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")
+        ),
+        launch_arguments={"gz_args": gz_world_arg}.items(),
+    )
+
+    # Spawn the URDF robot model into Gazebo
+    gz_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=[
+            "-topic",
+            "/robot_description",
+            "-name",
+            "rover",
+            "-allow_renaming",
+            "true",
+            "-x",
+            "0",
+            "-y",
+            "0",
+            "-z",
+            "0",
+        ],
+    )
+
+    gz_ros2_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist",
+            "/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock",
+            "/odometry/wheels@nav_msgs/msg/Odometry@ignition.msgs.Odometry",
+            "/tf@tf2_msgs/msg/TFMessage[ignition.msgs.Pose_V",
+            "/joint_states@sensor_msgs/msg/JointState[gz.msgs.Model",
+            "/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan",
+            "/imu/data@sensor_msgs/msg/Imu@gz.msgs.IMU",
+        ],
+    )
+
+    # Start the robot_state_publisher
+    params = {"use_sim_time": use_sim_time, "robot_description": robot_desc}
+    start_robot_state_publisher_cmd = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="both",
+        parameters=[params],
+        arguments=[],
+    )
+
+    # Start RViz
+    rviz_cmd = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=[
+            "-d",
+            os.path.join(
+                get_package_share_directory("rover_system"),
+                "rviz",
+                "urdf.rviz",
+            ),
+        ],
+    )
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_world_cmd)
+
+    # Gazebo
+    ld.add_action(gz_sim)
+    ld.add_action(gz_spawn_entity)
+    ld.add_action(gz_ros2_bridge)
+
+    # Robot state publisher
+    ld.add_action(start_robot_state_publisher_cmd)
+
+    # RViz
+    # ld.add_action(rviz_cmd)
+
+    return ld
