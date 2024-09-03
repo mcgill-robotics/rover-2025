faa0bb3c10771ff0c033940a7439d376df9d365e
helen512 <helendeng512@gmail.com>
2024-09-02 21:23:56 -0400

    migrate camera python script, msg

---
+++ cameras/scripts/camera_subscriber.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import os, sys
+from pydoc_data.topics import topics
+currentdir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(currentdir)
+import rospy2 as rospy
+currentdir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(currentdir)
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+
+class Node_CameraFrameSub():
+
+    def __init__(self):
+        self.frames = None 
+        rospy.init_node('camera_frame_subscriber')
+        self.camera_frame_subscriber = rospy.Subscriber('/camera_frames', Image, self.display_frames)
+
+
+    def display_frames(self, frame):
+        self.frames = self.ros_to_openCV_image(frame)
+        print(self.frames.shape)
+        cv2.imshow("Live Feed", self.frames)
+        cv2.waitKey(10)  # Look into to reduce latency
+
+    def ros_to_openCV_image(self, ros_image):
+        bridge = CvBridge()
+        cv_image = bridge.imgmsg_to_cv2(ros_image, desired_encoding='passthrough')
+        return cv_image
+
+
+
+if __name__ == "__main__":
+    driver = Node_CameraFrameSub()
+    rospy.spin()
