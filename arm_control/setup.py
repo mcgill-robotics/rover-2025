360dca240d623c6397ef9abdd029cde988695a24
Computer23000 <alex.zimo.zh@gmail.com>
2024-11-02 18:15:26 +0000

    attempt 4 at ros pkg

---
+++ arm_control/setup.py
@@ -0,0 +1,40 @@
+<<<<<<< Updated upstream:arm_control_old/setup.py
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages=["arm_control"],
+    package_dir={"": "src"},
+)
+
+setup(**setup_args)
+=======
+from setuptools import find_packages, setup
+
+package_name = 'arm_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='alex.zimo.zh@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
+>>>>>>> Stashed changes:arm_control/setup.py
