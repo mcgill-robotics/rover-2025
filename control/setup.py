ead6a0f871b75029fb589995ebcb4e9d2e242699
Aman Sidhu <55802966+Kuzcop@users.noreply.github.com>
2024-11-07 11:08:08 -0500

    Feat: drive_control ROS2 conversion
    * Created new drive_control package, and msg_srv interface package
    * Added drive_control_node.py
    * Updated drive_control
    * Integrated drive_control into control

---
+++ control/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mn297',
+    maintainer_email="martin.nguyen3@mail.mcgill.ca",
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'test_pub = test.pub:main',
+            'test_sub = test.sub:main',
+            'drive_control_node = scripts.drive_control_node:main'
+        ],
+    },
+)
