b71fbbeee48060dc6f5278848a0638286f2b7f22
Computer23000 <alex.zimo.zh@gmail.com>
2024-11-21 19:05:28 +0000

    Fix: changed variables names to be in snake case

--- arm_control/arm_control/human_arm_control.py
+++ arm_control/arm_control/human_arm_control.py
@@ -2,21 +2,21 @@ import numpy as np
 import math
 import arm_kinematics
 
-jointUpperLimits = [
+joint_upper_limits = [
     118.76 * np.pi / 180,
     90 * np.pi / 180,
     75 * np.pi / 180,
     75 * np.pi / 180,
     np.pi,
 ]  # rad
-jointLowerLimits = [
+joint_lower_limits = [
     -125.97 * np.pi / 180,
     -60 * np.pi / 180,
     -70 * np.pi / 180,
     -75 * np.pi / 180,
     -np.pi,
 ]  # rad
-jointMaxSpeed = [
+joint_max_speed = [
     np.pi/3,
     np.pi/3,
     np.pi/3,
@@ -31,7 +31,9 @@ joint_control_is_active = True
 def waist(joystick_input, cur_angle):
     #assumes joystick_input is normalized to be between -1.0 and 1.0
     #assumes speed to act as a scale factor (between 0.0 and 1.0)
-    cur_angle[0] = cur_angle[0] + speed * normalized_joystick_input * jointMaxSpeed[0]
+    cur_angle[0] = cur_angle[0] + speed * joystick_input * jointMaxSpeed[0]
+
+    #sets the waists angle to be at the limit if it exeeds the limit
     if cur_angle[0] > jointUpperLimits[0]:
         cur_angle[0] = jointUpperLimits[0]
     if cur_angle[0] < jointLowerLimits[0]:
