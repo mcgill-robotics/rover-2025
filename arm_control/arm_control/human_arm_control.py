32922c5e30d501fb7567dfc864da904ba7ece365
Flavie Qin <qinflavie@gmail.com>
2024-11-23 12:44:47 -0500

    feat: test cases

--- arm_control/arm_control/human_arm_control.py
+++ arm_control/arm_control/human_arm_control.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 import arm_kinematics
 
+#[waist, shoulder, elbow, wrist, hand(twist)]
 jointUpperLimits = [
     118.76 * np.pi / 180,
     90 * np.pi / 180,
@@ -24,13 +25,11 @@ joint_control_is_active = True
 def move_elbow(current, controller_input):
     """Returns list of new positions after moving elbow"""
     #assumes controller input is normalized between -1.0 and 1.0
-    current_position = current[2]
-    new_position = current_position * speed * controller_input
+    current_position = current[:]
+    new_position = current_position[2] + speed * controller_input
     if new_position >= jointUpperLimits[2]:
         new_position = jointUpperLimits[2]
     if new_position <= jointLowerLimits[2]:
         new_position = jointLowerLimits[2]
     current_position[2] = new_position
     return current_position
-    
-
--- arm_control/test/human_control_tests.py
+++ arm_control/test/human_control_tests.py
@@ -1,1 +1,23 @@
 from arm_control import human_arm_control
+
+jointUpperLimits = human_arm_control.jointUpperLimits
+jointLowerLimits = human_arm_control.jointLowerLimits
+
+
+
+##Tests for elbow flex
+
+#test that moving up works
+assert human_arm_control.move_elbow([1.0, 1.0, 1.0, 1.0, 1.0], 0.1) == [1.0, 1.0, 1.1, 1.0, 1.0]
+#test that moving down works
+assert human_arm_control.move_elbow([1.0, 1.0, 1.0, 1.0, 1.0], -0.1) == [1.0, 1.0, 0.9, 1.0, 1.0]
+
+#edge cases
+#test that doesnt move at upper limit
+assert human_arm_control.move_elbow([1.0, 1.0, jointUpperLimits[2], 1.0, 1.0], 0.1) == [1.0, 1.0, jointUpperLimits[2], 1.0, 1.0]
+#test that doesnt move at lower limit
+assert human_arm_control.move_elbow([1.0, 1.0, jointLowerLimits[2], 1.0, 1.0], -0.1) == [1.0, 1.0, jointLowerLimits[2], 1.0, 1.0]
+#test that stops at upper limit
+assert human_arm_control.move_elbow([1.0, 1.0, jointUpperLimits[2]-0.05, 1.0, 1.0], 0.1) == [1.0, 1.0, jointUpperLimits[2], 1.0, 1.0]
+#test that stops at lower limit
+assert human_arm_control.move_elbow([1.0, 1.0, jointLowerLimits[2]+0.05, 1.0, 1.0], -0.1) == [1.0, 1.0, jointLowerLimits[2], 1.0, 1.0]
