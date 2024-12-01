6ad645ef9aae48bbc0b82a08d45519ccc231427a
Mahan Maalekian <maalekianmahan@gmail.com>
2024-11-30 23:40:10 -0500

    integrate controller in drive control

--- react-ui/src/pages/Home.tsx
+++ react-ui/src/pages/Home.tsx
@@ -1,12 +1,8 @@
-import { Controller } from "../components/Controller";
 
 
 function Home() {
     return (
-        <div>
         <h2 style={{ height: "100%" }}>Home Page</h2>
-        <Controller/>
-        </div>
     );
 
 }
