ac8c3166805b67b527d3ef850d5a72976b36a021
Flavie Qin <qinflavie@gmail.com>
2025-01-18 12:55:41 -0500

    feat: define for wheel perpendicular

--- control/scripts/steering.py
+++ control/scripts/steering.py
@@ -1,3 +1,5 @@
+import math
+
 class Steering:
     def __init__(self, rWheel, base_length):
         self.rWheel=rWheel  # radius of wheel
@@ -12,3 +14,37 @@ class Steering:
         wRight=(vR+temp)/self.rWheel
         val = [wLeft, wRight]                     
         return val
+    
+    def rover_rotation(self, wheel_angles, joystick_input):
+        """
+        Returns the wheel speeds in array [TR, TL, BL, BR] needed for rover rotation left or right
+        Assumes joystick_input normalized between [-1, 1] for x-axis (L/R)
+        Wheel rotation speed is between [-1, 1] (backward, forward), value depends on joystick input depth
+        """
+
+        tolerance = math.pi/12 # value can be changed
+        # wheel angle according to unit circle form 0 to 2pi
+
+        # CASE 1: approximately perpendicular to rover
+        # front of wheel facing right
+        if abs(wheel_angles[0]) <= tolerance or abs(math.pi*2-wheel_angles[0]) <= tolerance: 
+            if joystick_input < 0: #turn left
+                # top wheels go backward, bottom wheel rotate forward
+                return [i*abs(joystick_input) for i in [-1, -1, 1, 1]] 
+            else: #turning right
+                # top wheels go forward, bottom wheel rotate backward
+                return [i*abs(joystick_input) for i in [1, 1, -1, -1]] 
+
+        # front of wheel facing left
+        elif abs(wheel_angles[0] - math.pi) <= tolerance:
+            pass
+
+            
+
+
+        # case 2: approximately parallel to rover
+
+        # case 3: / relative to rover
+
+        # case 4: \ relative to rover
+
