71ed0bc60fd07334413919cc7b3c4cea0d1ccde6
Ayush Patel <payush548@gmail.com>
2025-01-20 12:34:40 +0000

    Fixed angle logic issue

--- control/scripts/wheel_orient_rot.py
+++ control/scripts/wheel_orient_rot.py
@@ -22,7 +22,7 @@ def wheel_orientation_rot(x_input, y_input, curr_angle_rad):
         curr_angle_rad += 0.01
     
     while curr_angle_rad > new_angle_boundary:
-        curr_angle_rad = 0 if curr_angle_rad <= 0 else curr_angle_rad
+        curr_angle_rad = 2*math.pi if curr_angle_rad <= 0 else curr_angle_rad
         curr_angle_rad -= 0.01
 
     curr_angle_rad = round(curr_angle_rad, 2)
@@ -34,4 +34,5 @@ def wheel_orientation_rot(x_input, y_input, curr_angle_rad):
 print(wheel_orientation_rot(0,1,3*(math.pi/2))) #[pi/2,pi/2,pi/2,pi/2]
 print(wheel_orientation_rot(-1,0, 0)) #[pi,pi,pi,pi]
 print(wheel_orientation_rot(0.5,0.5, math.pi/2)) # [pi/4, pi/4,pi/4,pi/4]
+
 
