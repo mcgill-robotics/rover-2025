7e373b184397094e5c973b194624735eba80ff21
Ayush Patel <payush548@gmail.com>
2025-01-31 18:06:06 +0000

    feat: wheel orientation rotation with tests

--- control/scripts/steering.py
+++ control/scripts/steering.py
@@ -1,3 +1,6 @@
+import math
+import numpy as np
+
 class Steering:
     def __init__(self, rWheel, base_length):
         self.rWheel=rWheel  # radius of wheel
@@ -12,3 +15,44 @@ class Steering:
         wRight=(vR+temp)/self.rWheel
         val = [wLeft, wRight]                     
         return val
+
+    # rotates the wheels with joystick inputs
+    def wheel_orientation_rot(self, x_input, y_input, curr_angle_rad):
+        joystick_angle_rad = math.atan2(y_input, x_input)
+        new_angle_boundary = joystick_angle_rad
+        if new_angle_boundary < 0:
+            new_angle_boundary += 2* math.pi
+        tolerance = 0.000001
+        step_size = 0.1
+
+        #choose if more optimal to go cw or ccw and increment by 0.1 rad till desired angle
+        angle_diff = new_angle_boundary - curr_angle_rad
+        if abs(angle_diff) < tolerance:
+            return np.full(4,curr_angle_rad)
+        
+        elif 0 < angle_diff <= math.pi:
+            while abs(curr_angle_rad-new_angle_boundary) > tolerance:
+                if curr_angle_rad >= 2*math.pi:
+                    curr_angle_rad -= 2*math.pi
+                curr_angle_rad += step_size
+
+        elif angle_diff > math.pi:
+            while abs(curr_angle_rad-new_angle_boundary) > tolerance:
+                if curr_angle_rad <= 0 :
+                    curr_angle_rad += 2*math.pi
+                curr_angle_rad -= step_size
+
+        elif angle_diff < -math.pi:
+            while abs(curr_angle_rad-new_angle_boundary) > tolerance:
+                if curr_angle_rad >= 2* math.pi:
+                    curr_angle_rad -= 2*math.pi
+                curr_angle_rad += step_size
+        
+        elif -math.pi < angle_diff < 0:
+            while abs(curr_angle_rad-new_angle_boundary) > tolerance:
+                if curr_angle_rad <= 0:
+                    curr_angle_rad += 2*math.pi
+                curr_angle_rad -= step_size
+        
+        return np.full(4, round(curr_angle_rad,2))
+
--- control/scripts/wheel_orient_rot.py
+++ control/scripts/wheel_orient_rot.py
@@ -1,38 +1,46 @@
 import math
 import numpy as np
 
-def convert_joystick_to_angle(x_input, y_input):
-    # x_input, y_input are the joystick inputs, range [-1,1]
-    # x_input is joystick left/right
-    # y_input is joystick up/down
-    # x_input and y_input are in the range [-1,1]
-    angle_rad = math.atan2(y_input, x_input)
-    return angle_rad
 
-
-# This function updates wheel orientation based on joystick input
-def wheel_orientation_rot(x_input, y_input, curr_angle_rad):
-    joystick_angle_rad = convert_joystick_to_angle(x_input, y_input)
+    # rotates the wheels with joystick inputs
+def wheel_orientation_rot(self, x_input, y_input, curr_angle_rad):
+    joystick_angle_rad = math.atan2(y_input, x_input)
     new_angle_boundary = joystick_angle_rad
+    if new_angle_boundary < 0:
+        new_angle_boundary += 2* math.pi
+    tolerance = 0.000001
+    step_size = 0.1
 
-
-    #Very fast right now, could change...
-    while curr_angle_rad < new_angle_boundary:
-        curr_angle_rad = 0 if curr_angle_rad >= 2*math.pi else curr_angle_rad
-        curr_angle_rad += 0.01
+    #choose if more optimal to go cw or ccw and increment by 0.1 rad till desired angle
+    angle_diff = new_angle_boundary - curr_angle_rad
+    if abs(angle_diff) < tolerance:
+        return np.full(4,curr_angle_rad)
     
-    while curr_angle_rad > new_angle_boundary:
-        curr_angle_rad = 2*math.pi if curr_angle_rad <= 0 else curr_angle_rad
-        curr_angle_rad -= 0.01
+    elif 0 < angle_diff <= math.pi:
+        while abs(curr_angle_rad-new_angle_boundary) > tolerance:
+            if curr_angle_rad >= 2*math.pi:
+                curr_angle_rad -= 2*math.pi
+            curr_angle_rad += step_size
 
-    curr_angle_rad = round(curr_angle_rad, 2)
-    
-    
-    return np.full(4, curr_angle_rad)
+    elif angle_diff > math.pi:
+        while abs(curr_angle_rad-new_angle_boundary) > tolerance:
+            if curr_angle_rad <= 0 :
+                curr_angle_rad += 2*math.pi
+            curr_angle_rad -= step_size
 
-#Tests
-print(wheel_orientation_rot(0,1,3*(math.pi/2))) #[pi/2,pi/2,pi/2,pi/2]
-print(wheel_orientation_rot(-1,0, 0)) #[pi,pi,pi,pi]
-print(wheel_orientation_rot(0.5,0.5, math.pi/2)) # [pi/4, pi/4,pi/4,pi/4]
+    elif angle_diff < -math.pi:
+        while abs(curr_angle_rad-new_angle_boundary) > tolerance:
+            if curr_angle_rad >= 2* math.pi:
+                curr_angle_rad -= 2*math.pi
+            curr_angle_rad += step_size
+    
+    elif -math.pi < angle_diff < 0:
+        while abs(curr_angle_rad-new_angle_boundary) > tolerance:
+            if curr_angle_rad <= 0:
+                curr_angle_rad += 2*math.pi
+            curr_angle_rad -= step_size
+    
+    return np.full(4, round(curr_angle_rad,2))
+
 
 
