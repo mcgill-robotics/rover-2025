43c8684f3ab103bd17e1308baa8eb9362acd81ad
Aman Sidhu <asidhu_ca@yahoo.ca>
2025-03-01 00:02:19 -0500

    Added code to pass wheelspeeds to UDP socket for electrical integration (TEMP Solution)

--- control/scripts/firmware_node.py
+++ control/scripts/firmware_node.py
@@ -9,11 +9,26 @@ from rclpy.node import Node
 from msg_srv_interface.msg import GamePadInput
 from steering import rover_rotation , wheel_orientation_rot
 import math
+import numpy as np
+
+
+### TEMP for Drive Test ###
+import socket
+import pygame
+import time
+
+
+JETSON_IP = "192.168.0.101"  # IP of the motor computer
+UDP_PORT = 5005           # Port to send data
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP socket
+
+def send_UDP_message(msg):
+    sock.sendto(msg.encode(), (JETSON_IP, UDP_PORT))
+### TEMP for Drive Test ###
 
 class firmware(Node):
     def __init__(self):
 
-        #Initialize ROS node
         super().__init__("firmware_node")
 
         #Declare fields corresponging to controller input
@@ -22,7 +37,9 @@ class firmware(Node):
         #Declare field corresponding to speed control node and current state of wheels
         self.speed_controller = speed_controller()
 
-        self.deadzone = 0.1 # TODO: Tune value
+        # TODO: Tune values
+        self.deadzone = 0.1 
+        self.turning_speed = 4
 
         #TODO: Update code with API calls.
         #Call electrical API to get current state of wheels
@@ -40,7 +57,7 @@ class firmware(Node):
 
         #Speed given button input
         speed = self.speed_controller.updateSpeed(self.gamepad_input.x_button, self.gamepad_input.o_button)
-        # self.get_logger().info(f"Speed: {speed}")
+        speed = [speed for _ in range(4)]
         
         #Check whether there is an input value for rover rotation
         if self.gamepad_input.r1_button or self.gamepad_input.l1_button:
@@ -52,9 +69,8 @@ class firmware(Node):
 
             #Array with the desired speed for each wheel during rover rotation
             rotation_sp = rover_rotation(self.wheel_angles, rot_inp)
-            # self.get_logger().info(f"Rotation Angle: {rotation_sp}")
 
-            speed = rotation_sp*4
+            speed = [direction*4 for direction in rotation_sp]
             # TODO: Send speed to wheels
 
         #Check whether gears change
@@ -65,11 +81,14 @@ class firmware(Node):
         if self.not_in_deadzone_check(self.gamepad_input.l_stick_x, self.gamepad_input.l_stick_y):
             #Orientation for wheels given a joystick positinon
             self.wheel_angles = wheel_orientation_rot(self.gamepad_input.l_stick_x, self.gamepad_input.l_stick_y, self.wheel_angles[0])
-            # self.get_logger().info(f"wheel Angle: {self.wheel_angles}")
-            
+
             # TODO: Send new orientation to wheels
 
         # TODO: Use API to send input values for speed, orientation and rotation.
+
+        command = ":".join(map(str, speed))
+        self.get_logger().info(command)
+        send_UDP_message(command)
         
     def controller_callback(self, input: GamePadInput):
         self.gamepad_input = input
