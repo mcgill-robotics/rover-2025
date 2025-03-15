464b197a1daa5acab80767f090540338985d006d
Flavie Qin <qinflavie@gmail.com>
2025-03-15 13:47:41 -0400

    done function but not working as intended

--- arm_control/arm_control/human_arm_control.py
+++ arm_control/arm_control/human_arm_control.py
@@ -27,25 +27,52 @@ joint_max_speed = [
 ] # rad per method call
 
 speed = 1 # TO BE SET LATER
-speed_increment = 0.1 # TO BE SET LATER
-distance_increment = [0.05, 0.05/2, 0.05/4] # TO BE SET LATER
-current_cycle_mode = 0 # 0 = waist, 1 = shoulder, 2 = elbow, 3 = wrist, 4 = hand, 5 = claw
-joint_control_is_active = True 
+angle_increment = [np.pi/2, np.pi/4, np.pi/8] # TO BE SET LATER
 
 def upDownTilt(joystick_input, cur_angles):
-    #get current x,y,z,X,Y,Z of arms
-    cur_matrix = arm_kinematics.forwardKinematics(cur_angles)
-    cur_ee_pos = arm_kinematics.Mat2Pose(cur_matrix)
+    """Returns new angles needed to change tilt. Tries amount, then half, then half again. 
+    Assumes joystick_input is normalized between -1 and 1"""
 
+    #get current x,y,z,X,Y,Z of arms (XYZ are euler)
+    cur_matrix = arm_kinematics.forwardKinematics(cur_angles)
+    cur_pos = arm_kinematics.Mat2Pose(cur_matrix)
+    copy = cur_pos.copy()
+
+    for i in range(3):
+
+        cur_pos[4] = copy[4] + speed*angle_increment[i]*joystick_input  # modify Y euler angle (pitch)
+        try:
+            #use inverse kinematics to get positions of each joint needed for change of tilt
+            new_angles = arm_kinematics.inverseKinematics(cur_pos, cur_angles)
+
+            #if no exception raised, return
+            return new_angles
+        except Exception as e:
+            print(e)
+            pass
+
+    return cur_angles
+    
     #get current arm distance
     #arm as 2D line for x,y plane
-    projection_line = [
-        cur_ee_pos[0],
-        cur_ee_pos[1],
-    ]
+    #projection_line = [
+        #cur_ee_pos[0],
+        #cur_ee_pos[1],
+    #]
 
-    return target_pose 
+    #z is up (blue)
+    #y is side (green)
+    #x is in front (red)
+    #initial position is 0,0,0
+
+
+def test():
+    """Testing the tilt function!"""
+    cur_angles = [0,0,0,0,0]
+    print(cur_angles)
+    new_angles = upDownTilt(-0.6, cur_angles)
+    print(new_angles)
 
 
 if __name__ == "__main__":
-    print("hello")
+    test()
