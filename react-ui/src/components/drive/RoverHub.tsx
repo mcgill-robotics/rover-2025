8203433ca05e974fda5131e304f1da05d0f1d5bf
joshuazhou744 <joshuazhou744@gmail.com>
2025-03-23 15:26:37 -0400

    Create GPS component

--- react-ui/src/components/drive/RoverHub.tsx
+++ react-ui/src/components/drive/RoverHub.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import RoverInfo from './ui/RoverInfo';
 import RoverControl from './ui/RoverControl';
+import GPS from './ui/GPS';
 import './styles/RoverHub.css';
 
 interface RoverHubProps {
@@ -9,34 +10,38 @@ interface RoverHubProps {
 }
 
 const RoverHub: React.FC<RoverHubProps> = ({ streams, setStreams }) => {
-  const [isInfoMode, setIsInfoMode] = useState(true);
+  const [mode, setMode] = useState<"info"|"control"|"gps">("info");
 
   return (
     <div className="hub-view">
       <div className="hub">
         <div
-          className={`hub-toggle ${isInfoMode ? 'info-mode' : 'control-mode'}`}
+          className={`hub-toggle ${mode == "info" ? 'info-mode' : 'control-mode'}`}
         >
           <div
-            className={`hub-option ${isInfoMode ? 'active' : ''}`}
-            onClick={() => setIsInfoMode(true)}
+            className={`hub-option ${mode == "info" ? 'active' : ''}`}
+            onClick={() => setMode("info")}
           >
             Information
           </div>
           <div
-            className={`hub-option ${!isInfoMode ? 'active' : ''}`}
-            onClick={() => setIsInfoMode(false)}
+            className={`hub-option ${mode == "control" ? 'active' : ''}`}
+            onClick={() => setMode("control")}
           >
             Controls
+          </div>
+          <div
+            className={`hub-option ${mode == "gps" ? 'active' : ''}`}
+            onClick={() => setMode("gps")}
+          >
+            GPS
           </div>
         </div>
       </div> 
 
-      {isInfoMode ? (
-        <RoverInfo />
-      ) : (
-        <RoverControl streams={streams} setStreams={setStreams} />
-      )}
+      {mode == "info" && <RoverInfo />}
+      {mode == "control" && <RoverControl streams={streams} setStreams={setStreams} />}
+      {mode == "gps" && <GPS />}
     </div>
   );
 };
--- react-ui/src/components/drive/ui/GPS.tsx
+++ react-ui/src/components/drive/ui/GPS.tsx
@@ -2,7 +2,9 @@ import './styles/GPS.css';
 
 const GPS: React.FC = () => {
     return (
-        <div className="gps"></div>
+        <div className="gps">
+            gps
+        </div>
     )
 }
 
