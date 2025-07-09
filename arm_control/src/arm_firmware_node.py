f27af3a7b8055117010ae9dd752ee64e3f4418de
ida_ozd <idasuozdemir@gmail.com>
2025-07-09 19:56:05 -0400

    feat: arm firmware

--- arm_control/src/arm_firmware_node.py
+++ arm_control/src/arm_firmware_node.py
@@ -11,23 +11,11 @@ from std_msgs.msg import Float32MultiArray
 from std_msgs.msg import Bool
 
 
-# def run_motor_position(self, node: NodeID, position: float):
-#         self.esc.run(RunSpec.POSITION, position, self.motor_type, node)
-# Chnahe node to waist and the position is in degrees
-
-#     def calibrate_motor(self, node: NodeID):
-        # self.esc.run(RunSpec.CALIBRATION, 0.0, self.motor_type, node)
-        # the position you calibarate it at when you run motor position it starts from that degree
-
-
-        # get shit from arm_control_node 
-        # arm_control_cmd
-
 class arm_firmware(Node):
     def __init__(self):
         super.__init__("arm_firmware_node")
         self.position_subscriber = self.create_subscription(Float32MultiArray, "arm_position_cmd", self.broadcast_pos, 10) # Returns a list of wais,t shoulder, elbow, wrist, hand
-        self.fault_acknowledgement_subscriber = self.create_subscription(Bool, "acknowledge_faults", self.clear_motor_faults, 10)
+        self.faults_subscriber = self.create_subscription(Bool, "acknowledge_arm_faults", self.clear_motor_faults, 10)
 
         station = aCAN.CANStation(interface="slcan", channel="COM6", birate=500000)
         esc_interface = aCAN.ESCInterface(station)
@@ -35,17 +23,11 @@ class arm_firmware(Node):
         self.nodes = [aCAN.NodeID.WAIST, aCAN.NodeID.SHOULDER, aCAN.NodeID.ELBOW]
 
 
-    # def run(self):
-    #     self.publish_motor_position()
-    #     speeds_msg = Float32MultiArray()
-    #     speeds_msg.data = self.drive_speed_info
-    #     self.drive_motors_speeds_publisher.publish(speeds_msg)
+    def clear_motor_faults(self, acknowledge_faults: Bool):
+        if acknowledge_faults.data:
+            for motor in self.nodes:
+                self.arm_interfaces.acknowledge_motor_fault(motor)
 
-    #     if (self.pub_count % 5) == 0:
-    #         self.publish_motor_info()
-    #         self.pub_count = 0
-
-    #     self.pub_count += 1
 
     def broadcast_post(self, position_cmd: Float32MultiArray):
         """
@@ -58,7 +40,13 @@ class arm_firmware(Node):
             # self.arm_interface.stop_motor(node)  # Uncomment to stop motor
 
 
+def main(args=None):
+    rclpy.init(args=args)
+    node = arm_firmware()
+    rclpy.spin(node)
 
+if __name__ == "__main__":
+    main()
 
 
 
