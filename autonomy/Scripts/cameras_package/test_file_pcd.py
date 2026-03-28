a53e59c5806ec56c257a7f25ed5dc8231d29d68c
sebasresen <sebastian.es.en@gmail.com>
2026-03-28 12:33:28 -0400

    Added config lines to launch camera properly.

--- autonomy/Scripts/cameras_package/test_file_pcd.py
+++ autonomy/Scripts/cameras_package/test_file_pcd.py
@@ -37,6 +37,9 @@ config = rs.config()
 align = rs.align(rs.stream.color)
 pc = rs.pointcloud()
 
+
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 pipeline.start(config)
 os.makedirs("pointclouds", exist_ok=True)
 
